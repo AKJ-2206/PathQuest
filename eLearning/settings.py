8d9f1588b0f788ea03c4c9c7bb9269dfe390ba9e
Shruti Mall <shrutimall@Shrutis-MacBook-Air-2.local>
2024-08-30 09:51:29 +0530

    New File
    Former-commit-id: cf5f05a49bcf372451e794801d7e3cacd5f541ec

--- eLearning/settings.py
+++ eLearning/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'eLearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     'static/'
 ]
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
