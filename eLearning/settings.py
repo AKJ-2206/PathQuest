17bead0c214c5b6709543df78569e9a2f882bdf8
Shruti Mall <shrutimall@Shrutis-MacBook-Air-2.local>
2024-09-09 12:33:51 +0530

    New File
    Former-commit-id: af924b09b26493d6b0e4a2b95fcfa067f2f65a76

--- eLearning/settings.py
+++ eLearning/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,6 +69,9 @@ TEMPLATES = [
         },
     },
 ]
+
+
+
 
 WSGI_APPLICATION = 'eLearning.wsgi.application'
 
